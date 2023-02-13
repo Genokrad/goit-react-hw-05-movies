6993d249fdc3c9083859830654b2c9099144dc7b
Genokrad <zeeper90@gmail.com>
2023-02-13 11:58:08 +0200

    sada

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,5 @@
-// import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
-// import { HomePage } from 'Pages/HomePage/HomePage';
-// import MoviesPage from 'Pages/MoviesPage/MoviesPage';
-// import { MovieDetails } from './MovieDetails/MovieDetails';
-// import { Cast } from './Cast/Cast';
-// import { Reviews } from './Reviews/Reviews';
 import { lazy } from 'react';
 
 const HomePage = lazy(() => import('../Pages/HomePage/HomePage'));
